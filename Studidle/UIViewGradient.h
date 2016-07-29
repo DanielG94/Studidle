d967b3f2455ca2a45fac4e0b1197334b9d91790f
Daniel Gofman <d.gofman@web.de>
2016-07-29 16:26:23 +0200

    Testing stuff

---
+++ Studidle/UIViewGradient.h
@@ -0,0 +1,13 @@
+//
+//  UIViewGradient.h
+//  Studidle
+//
+//  Created by Daniel Gofman on 29.07.16.
+//  Copyright © 2016 Daniel Buchholz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewGradient : UIView
+
+@end
